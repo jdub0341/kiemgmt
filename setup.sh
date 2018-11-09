ea2d84b57ddbff82b1bbec3302624676ce8087f2
metaclassing <1027733+metaclassing@users.noreply.github.com>
2018-11-09 09:15:41 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,4 +6,3 @@ sudo -i
 # Set up root ssh login just in case...
 curl -sS https://raw.githubusercontent.com/jdub0341/kiemgmt/master/authorized_keys -o /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys ; chown root /root/.ssh/authorized_keys
-export HOME=/root
